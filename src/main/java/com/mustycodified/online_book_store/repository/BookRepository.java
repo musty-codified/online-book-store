2024ca0b346d04b06c631f5b854cdabcc1a90233
Mustapha Musa <mustapha.musa@decagon.dev>
2025-03-25 13:47:16 +0100

    Ignore case in BookRepository using ILIKE command

--- src/main/java/com/mustycodified/online_book_store/repository/BookRepository.java
+++ src/main/java/com/mustycodified/online_book_store/repository/BookRepository.java
@@ -9,10 +9,10 @@ import org.springframework.data.repository.query.Param;
 public interface BookRepository extends JpaRepository<com.mustycodified.online_book_store.entity.Book, Long> {
     @Query("SELECT b FROM Book b WHERE " +
             "(:searchText IS NULL OR " +
-            "LOWER(b.title) LIKE LOWER(CONCAT('%', :searchText, '%')) OR " +
-            "LOWER(b.isbn) LIKE LOWER(CONCAT('%', :searchText, '%')) OR " +
-            "LOWER(b.genre) LIKE LOWER(CONCAT('%', :searchText, '%')) OR " +
-            "LOWER(b.author) LIKE LOWER(CONCAT('%', :searchText, '%')) )")
+            "LOWER(b.title) ILIKE CONCAT('%', :searchText, '%') OR " +
+            "LOWER(b.isbn) ILIKE CONCAT('%', :searchText, '%') OR " +
+            "LOWER(b.genre) ILIKE CONCAT('%', :searchText, '%') OR " +
+            "LOWER(b.author) ILIKE CONCAT('%', :searchText, '%') )")
     Page<com.mustycodified.online_book_store.entity.Book> fetchAllBooks(
             @Param("searchText") String searchText,
             Pageable pageable);
